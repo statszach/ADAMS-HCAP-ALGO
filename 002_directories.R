219a7157244d893765b4d19faebf9cac343a305b
Zach Kunicki <zachary.kunicki@gmail.com>
2025-07-14 15:07:26 -0400

    first commit

---
+++ 002_directories.R
@@ -0,0 +1,9 @@
+user <- ""
+if (user == "Emma"){
+  data_filepath <- ""
+  save_filepath <- ""
+} else if (user == "zkunicki"){
+  data_filepath <- "R:\\BM_QuantitativeSciencesPrg\\STUDIES\\HRSADAMS\\data\\SOURCE"
+  rds_path <- "C:\\Users\\zkunicki\\OneDrive - Brown University\\Documents\\HomeMigration\\Research\\HRSADAMS\\PROJECTS\\ADAMS-HCAP-ALGO\\Images"
+  images_path <- "C:\\Users\\zkunicki\\OneDrive - Brown University\\Documents\\HomeMigration\\Research\\HRSADAMS\\PROJECTS\\ADAMS-HCAP-ALGO\\RDS"
+}
