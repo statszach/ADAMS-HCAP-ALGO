0bbe71b39ec3c468e0149866d9b58fa0e8e85534
Emma Nichols <emmanich@dts.usc.edu>
2025-08-18 11:44:52 -0700

    start of changes to make it work on Emma's computer

--- 002_directories.R
+++ 002_directories.R
@@ -1,10 +1,15 @@
-#user <- ""
+user <- "Emma"
 if (user == "Emma"){
-  data_filepath <- ""
-  save_filepath <- ""
+  data_filepath <- "C:\\Users\\emmanich\\P2AGING Dropbox\\Emma Nichols\\projects\\hcapalgorithm_adams\\data\\source\\"
+  rds_filepath <- "C:\\Users\\emmanich\\P2AGING Dropbox\\Emma Nichols\\projects\\hcapalgorithm_adams\\data\\derived\\"
+  images_filepath <- "C:\\Users\\emmanich\\P2AGING Dropbox\\Emma Nichols\\projects\\hcapalgorithm_adams\\plots\\"
+  code_filepath <- "C:\\Users\\emmanich\\code\\ADAMS-HCAP-ALGO\\"
 } else if (user == "zkunicki"){
   data_filepath <- "R:\\STUDIES\\HRSADAMS\\data\\SOURCE"
   rds_path <- "C:\\Users\\zkunicki\\OneDrive - Brown University\\Documents\\HomeMigration\\Research\\HRSADAMS\\PROJECTS\\ADAMS-HCAP-ALGO\\Images"
   images_path <- "C:\\Users\\zkunicki\\OneDrive - Brown University\\Documents\\HomeMigration\\Research\\HRSADAMS\\PROJECTS\\ADAMS-HCAP-ALGO\\RDS"
+  code_filepath <- ""
 }
+
+
 
